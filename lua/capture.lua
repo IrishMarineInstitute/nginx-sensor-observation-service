560a1484b153edccc59c3be50dd38f66262638af
Adam Leadbetter <aleadbetter9@gmail.com>
2015-11-20 16:05:11 +0000

    Split the Lua code to separate files
    Solves issue #1

---
+++ lua/capture.lua
@@ -0,0 +1,11 @@
+local capture = {}
+
+function capture(cmd, raw)
+	local f = assert(io.popen(cmd, 'r'))
+	local s = assert(f:read('*a'))
+	f:close()
+	if raw then return s end
+	return s
+end
+
+return capture
